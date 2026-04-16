3d33c78bcc4155b8beea6d7c69593e299ae9abb8
gxh <2036902768@qq.com>
2026-04-16 14:25:46 +0800

    feat: 自定义页面

---
+++ docs/.vitepress/theme/CustomLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <Layout>
+    <!-- 文档下面的评论 -->
+    <template #doc-after>
+      <div id="gitalk-container"></div>
+    </template>
+    <!-- 侧边导航栏，用来放广告位的 -->
+    <template #nav-screen-content-before>
+      111
+    </template>
+  </Layout>
+</template>
+
+<script lang="ts" setup>
+import DefaultTheme from "vitepress/theme";
+
+const {Layout} = DefaultTheme
+import { onMounted } from "vue";
+
+onMounted(() => {
+});
+</script>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,13 @@
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import Team from '../components/Team.vue'
+import Team from '../components/Team.vue';
+import Layout from "./CustomLayout.vue";
 import './styles/custom-block.css'
+import './styles/custom-common.css'
 
 export default {
     extends: DefaultTheme,
+    Layout,
     enhanceApp({ app }) {
         // 注册自定义全局组件
         app.component('Team', Team)
