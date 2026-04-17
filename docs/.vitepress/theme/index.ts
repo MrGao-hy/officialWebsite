5cd455a3c99d2bca55040331088740d0bc5de0da
gxh <2036902768@qq.com>
2026-04-17 13:32:04 +0800

    feat: 导入ant组件和系统组件

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,12 @@
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import Team from '../components/Team.vue';
+import Systems from '../components/Systems.vue';
 import Layout from "./CustomLayout.vue";
 import './styles/custom-block.css'
 import './styles/custom-common.css'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
 
 export default {
     extends: DefaultTheme,
@@ -11,5 +14,7 @@ export default {
     enhanceApp({ app }) {
         // 注册自定义全局组件
         app.component('Team', Team)
+        app.component('Systems', Systems)
+        app.use(Antd);
     }
 } satisfies Theme
